8ea91bc35dcb659671058ec67f1673f9a2c3c3c8
teeaychem <bsparkes@stanford.edu>
2024-06-13 21:55:30 +0100

    quite the complexity

---
+++ grammar.js
@@ -0,0 +1,26 @@
+module.exports = grammar({
+    name: 'DIMACS',
+
+    rules: {
+        source_file: $ => repeat(choice($.comment,
+                                        $.problem,
+                                        $.clause,
+                                       )),
+
+        negation: $ => '-',
+
+        variable: $ => /[1-9][0-9]*/,
+
+        literal: $ => seq(optional($.negation), $.variable),
+
+        comment: $ => seq('c', /[^\n]*/),
+
+        problem: $ => seq('p', 'cnf', $.variable, $.variable),
+
+        clause: $ => seq(repeat($.literal), '0'),
+
+
+
+
+  }
+});
